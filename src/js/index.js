bc125ae64f9b5bff64dccc96793624320980278a
Alamin <codepapa360@gmail.com>
2023-04-21 20:47:37 +0600

    added screenshots

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,17 @@
 "use strict";
 import "../sass/main.scss";
+
+const darkModeToggle = document.getElementById("dark-mode-checkbox");
+const root = document.documentElement;
+
+//dark mode feature
+const darkMode = function () {
+  if (darkModeToggle.checked === true) {
+    root.classList.add("dark-mode");
+  } else {
+    root.classList.remove("dark-mode");
+  }
+};
+
+darkModeToggle.addEventListener("change", darkMode);
+window.addEventListener("load", darkMode);
